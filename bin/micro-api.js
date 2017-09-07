5e2ea391a860716840890f0e056163332a8b13c2
Sirajul <rianaville@gmail.com>
2017-09-07 19:40:51 +0600

    change to babel converter

--- bin/micro-api.js
+++ bin/micro-api.js
@@ -3,6 +3,12 @@
 var program = require('commander')
 const Koa = require('koa')
 const glob = require('glob')
+var babel = require('babel-core')
+var fs = require('fs')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const rimraf = require('rimraf')
+
 global.appRoot = process.env.PWD
 
 program
@@ -15,14 +21,51 @@ var host = !!program.host ? program.host : '0.0.0.0',
   port = !!program.port ? program.port : 8080
 
 const app = new Koa()
-glob("app/api/*.js", null, function (er, files) {
-  files.forEach(f => {
-    var Cls = require(`${appRoot}/${f}`).default
-    var cls = new Cls()
-    app.use(cls.generate())
+
+function mkdir(dir) {
+  try {
+    fs.mkdirSync(dir)
+  } catch (e) {
+    if (e.errno === 34) {
+      mkdir(path.dirname(dir))
+      mkdir(dir)
+    }
+  }
+}
+rimraf.sync(appRoot + '/build')
+var files = glob.sync("app/**/*.js")
+files.forEach(f => {
+  var {
+    code
+  } = babel.transformFileSync(f, {
+    plugins: [
+      "transform-es2015-modules-commonjs", [
+        "module-resolver",
+        {
+          "root": [
+            "./app"
+          ],
+          "alias": {
+            "helper": "./app/helpers",
+            "model": "./app/models"
+          }
+        }
+      ]
+    ]
   })
-  app.listen(port, () => console.log(`Server Started @ ${port}`))
+  var filepath = path.dirname(appRoot + '/build/' + f)
+  // fs.mkdirSync(filepath)
+  console.log(filepath)
+  mkdirp.sync(filepath)
+
+  fs.writeFileSync(appRoot + '/build/' + f, code)
 })
+var apis = glob.sync(appRoot + '/build/app/api/*.js')
+apis.forEach(f => {
+  var Cls = require(f)
+  var cls = new Cls()
+  app.use(cls.generate())
+})
+app.listen(port, () => console.log(`Server Started @ ${port}`))
 
 process.on('SIGINT', process.exit)
-
