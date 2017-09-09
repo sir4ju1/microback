c5d62575712fd6fd9117b8ea9a9dfc44e1d232af
Sirajul <rianaville@gmail.com>
2017-09-09 18:03:31 +0600

    path changed

--- config/paths.js
+++ config/paths.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const rootPath = path.resolve(process.cwd())
 const buildPath = path.join(rootPath, 'build')
-
+const publicBuildPath = path.join(buildPath, 'public')
 module.exports = {
   rootPath,
   buildPath,
-  appPath: path.join(rootPath, 'app'),
+  publicBuildPath, 
+  publicSrcPath: path.join(rootPath, 'public'),
   serverSrcPath: path.join(rootPath, 'src'),
   serverBuildPath: buildPath,
+  publicPath: '/',
   userNodeModulesPath: path.join(rootPath, 'node_modules'),
+  serverUrl: 'http://localhost:3000'
 }
