4fdec20a2cff7d3fd3543ac39554ec928788f7e8
Sirajul <rianaville@gmail.com>
2017-09-09 11:21:43 +0600

    path correction in indexjs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const Koa = require('koa')
 // const glob = require('glob')
 
 const app = new Koa()
-// var appRoot = require('path').resolve(process.cwd())
+var appRoot = require('path').resolve(process.cwd())
 // let routes = []
-const normalizedPath = require('path').join(__dirname, '../app/api')
+const normalizedPath = require('path').join(appRoot, 'app/api')
 require('fs').readdirSync(normalizedPath).forEach(function (file) {
-  var Cls = require(`../app/api/${file}`).default
+  var Cls = require(`${normalizedPath}/${file}`).default
   var cls = new Cls()
   app.use(cls.generate())
 })
