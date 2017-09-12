840eded8d5927d62effb3010224fe803cb0df827
Sirajul <rianaville@gmail.com>
2017-09-12 15:50:55 +0600

    exporting function noauth

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require('koa')
 
 const app = new Koa()
 
-const { RestGen, route } = require('./src/restgen')
+const { RestGen, route, noauth } = require('./src/restgen')
 const auth = require('./src/auth')
 
-module.exports = { app, RestGen, route, auth }
+module.exports = { app, RestGen, route, noauth, auth }
