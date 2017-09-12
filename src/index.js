02700a59ffa360e55c1d1652e5c10239dd1461ab
Sirajul <rianaville@gmail.com>
2017-09-12 15:44:25 +0600

    app jwt right position

--- src/index.js
+++ src/index.js
@@ -12,12 +12,15 @@ const host = process.env.HOST || '0.0.0.0'
 
 const normalizedPath = path.join(config.rootPath, 'app/api')
 var noAuthList = []
+var routes = []
+
 require('fs').readdirSync(normalizedPath).forEach(function (file) {
   var Cls = require(`../../../app/api/${file}`).default
   var cls = new Cls()
-  app.use(cls.generate())
+  routes.push(cls.generate())
   noAuthList.concat(cls.getNoAuths())
 })
+
 app.use(cors())
 if (process.env.AUTH) {
   app.use(jwt({
@@ -26,5 +29,6 @@ if (process.env.AUTH) {
     path: noAuthList
   }))
 }
+routes.forEach(r => app.use(r))
 
 app.listen(port, () => console.log(chalk.bgGreen('  OK  '), chalk.green(`Server started`), chalk.dim(`[http://localhost:${port}]`)))
