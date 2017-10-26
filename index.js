f2a3b33e18b80ccc436ae64a00de1876b1800087
Sirajul <rianaville@gmail.com>
2017-10-26 19:47:48 +0600

    path correction for ws

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { RestGen, route, noauth } = require('./lib/restgen')
-const { WsGen, ws } = require('./lib/restgen')
+const { WsGen, ws } = require('./lib/wsgen')
 const auth = require('./lib/auth')
 const model = require('./lib/model')
 const server = function () {
